0252b0853be1e0f1c96cf2dac6d4ca2ce44a7799
Maximilian Brunner <mabrunne@student.ethz.ch>
2016-11-25 14:54:22 -0800

    Corrected model, updated logging

--- simModel.jl
+++ simModel.jl
@@ -10,16 +10,16 @@ function simKinModel(z::Array{Float64},u::Array{Float64},dt::Float64,coeff::Arra
     zNext::Array{Float64}
     L_a = modelParams.l_A
     L_b = modelParams.l_B
-    c = ([z[1]^4 z[1]^3 z[1]^2 z[1] 1]*coeff)[1]        # Polynomial
+    c = ([z[1]^8 z[1]^7 z[1]^6 z[1]^5 z[1]^4 z[1]^3 z[1]^2 z[1] 1]*coeff)[1]        # Polynomial
 
-    bta = atan(L_a/(L_a+L_b)*tan(u[2]))
+    bta = atan(L_a/(L_a+L_b)*tan(u[2]+abs(u[2])*u[2]))
     dsdt = z[4]*cos(z[3]+bta)/(1-z[2]*c)
 
-    zNext = z
-    zNext[1] = z[1] + dt*dsdt
-    zNext[2] = z[2] + dt*z[4] * sin(z[3] + bta)
-    zNext[3] = z[3] + dt*(z[4]/L_a*sin(bta)-dsdt*c)
-    zNext[4] = z[4] + dt*(u[1] - modelParams.c_f*abs(z[4])*z[4])
+    zNext = copy(z)
+    zNext[1] = z[1] + dt*dsdt                               # s
+    zNext[2] = z[2] + dt*z[4] * sin(z[3] + bta)             # eY
+    zNext[3] = z[3] + dt*(z[4]/L_a*sin(bta)-dsdt*c)         # ePsi
+    zNext[4] = z[4] + dt*(u[1] - modelParams.c_f*z[4])      # v
 
     return zNext
 end
@@ -66,27 +66,25 @@ function simDynModel(z::Array{Float64},u::Array{Float64},dt::Float64,coeff::Arra
     dsdt = (z[1]*cos(z[4]) - z[2]*sin(z[4]))/(1-z[5]*c)
 
     zNext = copy(z)
-    zNext[1] = z[1] + dt * (z[7] + z[2]*z[3] - c_f*z[1]^2*sign(z[1]))       # xDot
+    zNext[1] = z[1] + dt * (z[7] + z[2]*z[3] - c_f*z[1])                    # xDot
     zNext[2] = z[2] + dt * (2/m*(FyF*cos(z[8]) + FyR) - z[3]*z[1])          # yDot
     zNext[3] = z[3] + dt * (2/I_z*(L_f*FyF - L_r*FyR))                      # psiDot
     zNext[4] = z[4] + dt * (z[3]-dsdt*c)                                    # ePsi
     zNext[5] = z[5] + dt * (z[1]*sin(z[4]) + z[2]*cos(z[4]))                # eY
     zNext[6] = z[6] + dt * dsdt                                             # s
-    zNext[7] = z[7] + dt * (u[1] - z[7]) * 0.1/dt                           # a
-    zNext[8] = z[8] + dt * (u[2] - z[8]) * 0.1/dt                           # d_f
+    zNext[7] = z[7] + dt * (u[1] - z[7]) * 100                              # a
+    zNext[8] = z[8] + dt * (u[2] - z[8]) * 10                               # d_f
 
     return zNext
 end
 
 function pacejka(a)
-    B = 1.0#20
+    B = 1.0             # This value determines the steepness of the curve
     C = 1.25
-    mu = 0.8
+    mu = 0.8            # Friction coefficient (responsible for maximum lateral tire force)
     m = 1.98
     g = 9.81
     D = mu * m * g/2
-    D = D*10.0
-
     C_alpha_f = D*sin(C*atan(B*a))
     return C_alpha_f
 end
@@ -110,9 +108,9 @@ function simDynModel_xy(z::Array{Float64},u::Array{Float64},dt::Float64,modelPar
     m   = modelParams.m
     I_z = modelParams.I_z
 
-    a_F = 0
-    a_R = 0
-    if abs(z[3]) > 0.1
+    a_F = 0.0
+    a_R = 0.0
+    if abs(z[3]) > 0.2
         a_F     = atan((z[4] + L_f*z[6])/abs(z[3])) - z[8]
         a_R     = atan((z[4] - L_r*z[6])/abs(z[3]))
     end
@@ -128,12 +126,13 @@ function simDynModel_xy(z::Array{Float64},u::Array{Float64},dt::Float64,modelPar
     # compute next state
     zNext[1]        = zNext[1]       + dt * (cos(z[5])*z[3] - sin(z[5])*z[4])               # x
     zNext[2]        = zNext[2]       + dt * (sin(z[5])*z[3] + cos(z[5])*z[4])               # y
-    zNext[3]        = zNext[3]       + dt * (z[7] + z[4]*z[6] - 0.63*z[3]^2*sign(z[3]))     # v_x
+    zNext[3]        = zNext[3]       + dt * (z[7] + z[4]*z[6] - 0.5*z[3])                   # v_x
     zNext[4]        = zNext[4]       + dt * (2/m*(FyF*cos(z[8]) + FyR) - z[6]*z[3])         # v_y
     zNext[5]        = zNext[5]       + dt * (z[6])                                          # psi
     zNext[6]        = zNext[6]       + dt * (2/I_z*(L_f*FyF - L_r*FyR))                     # psiDot
-    zNext[7]        = zNext[7]       + dt * (u[1]-z[7])*0.1/dt                              # a
-    zNext[8]        = zNext[8]       + dt * (u[2]-z[8])*0.1/dt                              # d_f
+    zNext[7]        = zNext[7]       + dt * (u[1]-z[7])*100                                 # a
+    zNext[8]        = zNext[8]       + dt * (u[2]-z[8])*100                                 # d_f
 
+    zNext[3] = max(0,zNext[3])              # limit speed to positive values (BARC specific)
     return zNext, [a_F a_R]
 end
